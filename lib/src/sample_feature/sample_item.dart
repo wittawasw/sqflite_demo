42ca6bdaf395ac4c89848d484363848af58d8b87
wittawasw <start@wittawasw.com>
2024-09-15 05:32:58 +0700

    define sampleitem

--- lib/src/sample_feature/sample_item.dart
+++ lib/src/sample_feature/sample_item.dart
@@ -1,6 +1,8 @@
-/// A placeholder class that represents an entity or model.
 class SampleItem {
-  const SampleItem(this.id);
-
   final int id;
+  final String name;
+  final String description;
+  final int price;
+
+  const SampleItem(this.id, this.name, this.description, this.price);
 }
--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -8,7 +8,7 @@ import 'sample_item_details_view.dart';
 class SampleItemListView extends StatelessWidget {
   const SampleItemListView({
     super.key,
-    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+    this.items = const [SampleItem(1, "", "", 1)],
   });
 
   static const routeName = '/';
@@ -49,21 +49,20 @@ class SampleItemListView extends StatelessWidget {
           final item = items[index];
 
           return ListTile(
-            title: Text('SampleItem ${item.id}'),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-            ),
-            onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(
-                context,
-                SampleItemDetailsView.routeName,
-              );
-            }
-          );
+              title: Text('SampleItem ${item.id}'),
+              leading: const CircleAvatar(
+                // Display the Flutter Logo image asset.
+                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+              ),
+              onTap: () {
+                // Navigate to the details page. If the user leaves and returns to
+                // the app after it has been killed while running in the
+                // background, the navigation stack is restored.
+                Navigator.restorablePushNamed(
+                  context,
+                  SampleItemDetailsView.routeName,
+                );
+              });
         },
       ),
     );
