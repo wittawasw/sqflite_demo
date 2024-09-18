b7d110cb35965ca2293cf73160393b8b1d4bbe0f
wittawasw <start@wittawasw.com>
2024-09-19 00:43:53 +0700

    restore comment

--- lib/src/home/screen.dart
+++ lib/src/home/screen.dart
@@ -14,6 +14,9 @@ class HomeScreen extends StatelessWidget {
         IconButton(
           icon: const Icon(Icons.settings),
           onPressed: () {
+            // Navigate to the settings page. If the user leaves and returns
+            // to the app after it has been killed while running in the
+            // background, the navigation stack is restored.
             Navigator.restorablePushNamed(context, SettingsView.routeName);
           },
         ),
