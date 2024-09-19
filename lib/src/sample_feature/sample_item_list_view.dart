df5541de48a7625b883e917f7751bc8d3fbf1c41
wittawasw <start@wittawasw.com>
2024-09-20 01:24:57 +0700

    refactor listview

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -31,49 +31,55 @@ class _SampleItemListViewState extends State<SampleItemListView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Items'),
-      ),
-      body: _controller.items.isEmpty
-          ? const Center(child: Text('No items found.'))
-          : ListView.builder(
-              itemCount: _controller.items.length,
-              itemBuilder: (context, index) {
-                final item = _controller.items[index];
-                return ListTile(
-                  title: Text(item.name),
-                  subtitle: Text(item.description),
-                  trailing: Text(item.price.toString()),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SampleItemDetailsView(
-                            id: item.id, onDelete: _loadItems),
-                      ),
-                    ).then((_) {
-                      _loadItems();
-                    });
-                  },
-                );
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newItem = await Navigator.push<SampleItem>(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const SampleItemNewView(),
-            ),
-          );
+        appBar: AppBar(
+          title: const Text('Sample Items'),
+        ),
+        body: _controller.items.isEmpty
+            ? const Center(child: Text('No items found.'))
+            : ListView.builder(
+                itemCount: _controller.items.length,
+                itemBuilder: (context, index) {
+                  return sampleItemTile(_controller.items[index]);
+                },
+              ),
+        floatingActionButton: newSampleItemBtn());
+  }
 
-          if (newItem != null) {
-            await _controller.addItem(newItem);
-            _loadItems();
-          }
-        },
-        child: const Icon(Icons.add),
-      ),
+  Widget sampleItemTile(SampleItem item) {
+    return ListTile(
+      title: Text(item.name),
+      subtitle: Text(item.description),
+      trailing: Text(item.price.toString()),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                SampleItemDetailsView(id: item.id, onDelete: _loadItems),
+          ),
+        ).then((_) {
+          _loadItems();
+        });
+      },
+    );
+  }
+
+  Widget newSampleItemBtn() {
+    return FloatingActionButton(
+      onPressed: () async {
+        final newItem = await Navigator.push<SampleItem>(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SampleItemNewView(),
+          ),
+        );
+
+        if (newItem != null) {
+          await _controller.addItem(newItem);
+          _loadItems();
+        }
+      },
+      child: const Icon(Icons.add),
     );
   }
 }
